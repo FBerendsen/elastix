85666c3e18c0fc60718679d0c9d9b13bc195c823
Floris Berendsen <floris.berendsen@gmail.com>
2017-08-22 15:18:07 +0200

    ENH: Properly set gridSpacingSchedule
    The gridSpacingSchedule in elxParameterObject was set to factors of sqrt(2), but was using a poorly rounded 1.41^n.
    Why the gridSpacingSchedule should be different than the elastix default (factors of 2) is not clear to me, though.

--- Core/Main/elxParameterObject.cxx
+++ Core/Main/elxParameterObject.cxx
@@ -349,9 +349,10 @@ ParameterObject
   if( transformName == "bspline" || transformName == "nonrigid" || transformName == "groupwise" ) // <-- nonrigid for backwards compatibility
   {
     ParameterValueVectorType gridSpacingSchedule = ParameterValueVectorType();
-    for( double resolution = 0; resolution < numberOfResolutions; ++resolution )
+    for( unsigned int resolution = 0; resolution < numberOfResolutions; ++resolution )
     {
-      gridSpacingSchedule.insert( gridSpacingSchedule.begin(), ToString( pow( 1.41, resolution ) ) );
+      // Set spacing schedule to factors of sqrt(2)
+      gridSpacingSchedule.insert( gridSpacingSchedule.begin(), ToString( double( 1 << ( resolution / 2 ) ) * ( resolution % 2 ? sqrt( 2.0 ) : 1.0 ) ) );
     }
 
     parameterMap[ "GridSpacingSchedule" ]             = gridSpacingSchedule;
